05d8030836b464ffdaa72b90059aa437abf2dd0c
fofo <zhongpei@vip.qq.com>
2016-10-11 20:31:04 +0800

    fix

--- endpoints/main.py
+++ endpoints/main.py
@@ -1,17 +1,20 @@
 #!coding: utf-8
 """
 Usage:
-    main.py <host> <username> <password> [-r]  [--port=<port>] [--hub=<hub>] [--pppoe-username=<username>] [--pppoe-password=<password>]
+    main.py <host> <username> <password> [-r]  [--port=<port>] [--hub=<hub>] [--pppoe-username=<username>] [--pppoe-password=<password>] [--output=<output>]
 
 Options:
     -h --help                   Show help
     -r                          Change route to make connect to Packetix Server always use default gw
+    --env                       Get param from env
     --port=<port>               Packetix Server Port [default: 15555].
     --hub=<hub>                 Packetix Server Hub [default: VPN]
     --pppoe-username=<username> PPPoE username
     --pppoe-password=<password> PPPoE password
+    --output=<output>           output file
 """
 
+import sys
 from docopt import docopt
 import time
 from command import Commander
@@ -48,31 +51,35 @@ def writeconf(template,target,**kw):
             return True
     return False
 
-def init_pppoe(args):
+def init_pppoe(args , status_loop = 10):
     if args.get("--pppoe-username") is None:
         logging.error("not have --pppoe-username")
-        return False
+        return False,"pppoe param error"
     if args.get('--pppoe-password') is None:
         logging.error("not have --pppoe-password")
-        return False
+        return False,"pppoe param error"
     ok = writeconf("/etc/ppp/peers/dsl-provider.tp","/etc/ppp/peers/dsl-provider",username=args["--pppoe-username"])
     if not ok:
         logging.error("write conf /etc/ppp/peers/dsl-provider failed")
+        return False,"write conf /etc/ppp/peers/dsl-provider failed"
     ok = writeconf("/etc/ppp/pap-secrets.tp","/etc/ppp/pap-secrets",username=args["--pppoe-username"],password=args['--pppoe-password'])
     if not ok:
         logging.error("write conf /etc/ppp/pap-secrets failed")
+        return False,"write conf /etc/ppp/pap-secrets failed"
 
     c = Commander()
     rd, ed = c.command2("pon dsl-provider")
     if len(ed) > 0:
         logging.error("pon failed")
-        return False
+        return False,"pon failed"
 
-    ok,why = is_pppoe_conneced()
-    while not ok:
+    for i in range(status_loop):
         ok,why = is_pppoe_conneced()
+        logging.info("pppoe {ok}({why})".format(ok=ok,why=why))
         if ok:
-            print ok,why
+            return True,why
+        time.sleep(1)
+    return False,"pppoe error"
 
 def is_pppoe_conneced():
     c = Commander()
@@ -84,28 +91,38 @@ def is_pppoe_conneced():
             return True,ip
     return False,"error"
 
-def init_vpn(args):
+def output(args,**kw):
+    output = args.get("--output")
+    if output is None:
+        return False
+    with open(output,"a+") as f:
+        for k,v in kw.items():
+            f.write("{key} : {value}\n".format(key=k,value=v))
+        f.write("\n")
+    return True
+
+def init_vpn(args, status_loop = 10):
     c = Commander()
     rd, ed = c.command2("service rsyslog start")
     if len(ed) > 0:
         logging.error("start rsyslog failed")
-        return False
+        return False,"start rsyslog failed"
     rd, ed = c.command2("/opt/vpnclient/vpnclient start")
     if len(ed) > 0:
         logging.error("start vpnclient failed")
-        return False
+        return False,"start vpnclient failed"
 
     time.sleep(1)
 
     ok, rd, ed = c.vpn_command("NicCreate p1")
     if not ok:
         logging.error("create nic failed")
-        return False
+        return False,"create nic failed"
 
     time.sleep(1)
     if 'vpn_p1' not in netifaces.interfaces():
         logging.error("create nic failed")
-        return False
+        return False,"create nic failed"
     
     ok, rd, ed = c.vpn_command(
             "AccountCreate {username} /SERVER:{host}:{port} /HUB:VPN /USERNAME:{username} /NICNAME:p1".format(
@@ -116,7 +133,7 @@ def init_vpn(args):
     )
     if not ok:
         logging.error("create account failed")
-        return False
+        return False,"create account failed"
 
     ok, rd, ed = c.vpn_command(
             "AccountPasswordSet {username} /PASSWORD:{password} /TYPE:standard".format(
@@ -126,7 +143,7 @@ def init_vpn(args):
     )
     if not ok:
         logging.error("account set password failed")
-        return False
+        return False,"account set password failed"
 
     ok, rd, ed = c.vpn_command(
             "AccountConnect {username} ".format(
@@ -135,14 +152,16 @@ def init_vpn(args):
     )
     if not ok:
         logging.error("connect failed")
-        return False
+        return False,"connect failed"
 
-    ok,why = is_vpn_connected()
-    logging.info("vpn connect %s (%s)"%(ok,why))
-    while not ok:
-        time.sleep(1)
+    for i in range(status_loop):
         ok,why = is_vpn_connected()
         logging.info("vpn connect %s (%s)"%(ok,why))
+        if ok:
+            return True,why
+        time.sleep(1)
+
+    return False,"vpn error"
 
 def is_vpn_connected():
     c = Commander()
@@ -167,9 +186,32 @@ def is_vpn_connected():
 
 if __name__ == '__main__':
     args = docopt(__doc__)
+    logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+            datefmt='%a, %d %b %Y %H:%M:%S'
+    )
     #print args
+    if args.get("--env"):
+        if os.getenv("HOST"):
+            args['<host>'] = os.getenv("HOST")
+        if os.getenv("USERNAME"):
+            args['<username>'] = os.getenv("USERNAME")
+        if os.getenv("PASSWORD"):
+            args['<password>'] = os.getenv("PASSWORD")
+        if os.getenv("PPPOE_USERNAME"):
+            args['--pppoe-username'] = os.getenv("PPPOE_USERNAME")
+        if os.getenv("PPPOE_PASSWORD"):
+            args['--pppoe-password'] = os.getenv("PPPOE_PASSWORD")
+    print args
 
-    ok = init_vpn(args)
-    ok = init_pppoe(args)
+    ok,why = init_vpn(args)
+    output(args,vpn=ok,status=why)
+
+    if ok: 
+        ok,why = init_pppoe(args)
+        output(args,pppoe=ok,ip=why)
+
     if args['-r']:
         add_route(args['<host>'])
+    sys.exit()
